a009373a65a6d9a9608f3ed89c6533b51eb3e094
Rafael Andrade <rafaandrade2810@gmail.com>
2024-02-26 19:13:40 -0300

    Criado o PersonRepository

---
+++ src/main/java/br/com/randrade/repositories/PersonRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.randrade.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.randrade.model.Person;
+
+/**
+ * 
+ */
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
