b11f8cc88a51b1a0aeaba89d6453c62a1681f0a9
Rafael Andrade <rafaandrade2810@gmail.com>
2024-02-28 02:58:20 -0300

    Refatorando o PersonController

--- src/main/java/br/com/randrade/controller/PersonController.java
+++ src/main/java/br/com/randrade/controller/PersonController.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,46 +29,36 @@ public class PersonController {
 	@Autowired
 	private PersonServices personServices;
 	
-
-	
-	@RequestMapping(value = "/{id}",
-			method = RequestMethod.GET,
+	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") Long id) {
 		
 		return personServices.findById(id);
 	}
 	
-	@RequestMapping(value = "/{id}",
-			method = RequestMethod.DELETE)
+	@GetMapping(value = "/{id}")
 	public void deletePerson(@PathVariable(value = "id") Long id) {
 		
 		personServices.deletePerson(id);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person createPerson(@RequestBody Person person) {
 		
 		return personServices.createPerson(person);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person updatePerson(@RequestBody Person person) {
 		
 		return personServices.updatePerson(person);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll(){
 		
 		return personServices.findAll();
-	}
-	
-	
-	
+	}	
 }
