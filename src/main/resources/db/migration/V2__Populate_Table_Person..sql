500a0fa445e6b367c0168095f294b1328b7e8233
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-03 12:53:35 -0300

    Implementação das migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person..sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Miguel', 'Male', 'Testes'),
+	(3, 'São Paulo', 'Nelson', 'Male', 'Juracir'),
+	(4, 'São Paulo', 'Nikola', 'Male', 'Tesla');
